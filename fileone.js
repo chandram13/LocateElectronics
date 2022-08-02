b8faccf0911948b82aec2b89ede4fbd8729fdbca
Marvish <mschandr@uci.edu>
2022-08-02 14:20:44 -0700

    Updated cell and tablet functions

--- fileone.js
+++ fileone.js
@@ -1,14 +1,46 @@
 
 class Phones:
 
+ioSProducts = ["iPhone 13", "iPhone 12","iPhone SE", "iPhone 11", "iPhone X"]
+samsungProducts = ["Samsung Galaxy S22","Samsung Galaxy S21","Samsung Galaxy Z Flip 3", "Samsung Galaxy Z Fold 3"]
+googleProducts = ["Google Pixel 6a","Google Pixel 6","Google Pixel 6 Pro"]
+
 
 class Computers:
 
+function majorComps: {
+    appleProducts = ["Macbook Air (M2)","Macbook Air (M1)","Macbook Pro 13 (M2)", "Macbook Pro 13 (M1)", "Macbook Pro 14","Macbook Pro 16"]
+    acerProducts = ["Aspire 3", "Aspire 5","Nitro 5","Predator","Chromebook"]
+    alienwareProducts = ["x15 R1","m15 R5", "m17 R5", "x17 R1", "x14 R1", "m15 R4", "m15 R7", "x15 R2"]
+    asusProducts = ["Vivobook","Zenbook","ROG","ROG Zephyrus","TUF Dash", "Vivobook Pro","Zenbook Pro","Chromebook"]
+    dellProducts = ["Inspiron","G15","XPS 13", "XPS 17","Latitude"]
+    gigabyteProducts = ["A5","Aero","Aorus","Aorus Level","U4","G5"]
+    HPproducts = ["Envy","Stream","Spectre","Omen","Pavilion","Elitebook","Victus"]
+    lenovoProducts = ["Yoga 7i","Ideapad","Yoga 6","Flex","Chromebook","Thinkpad","Legion"]
+    lgProducts = ["Gram 17","Gram 15","Gram WUXUGA"]
+    microsoftProducts = ["Surface","Surface Pro X","Surface 2-in-1"]
+    msiProducts = ["Modern 15","Prestige","Sword","Delta","Stealth","Vector","GP66","GF65"]
+    razerProducts = ["Blade 14","Blade 15","Book 13","Blade 17","Blade 15.6","Blade Stealth 13.3"]
+    samProducts = ["Galaxy Book Go","Galaxy Book 2 Pro","Galaxy Book Flex 2","Galaxy Book Pro 360","Chromebook", "Galaxy Book Odyssey"]
+}
 
 
+class Tablets:
 
-class Table
-ts:
+function majorTabs: {
+    appleTablets = ["iPad Air","iPad Pro","10.2in iPad","iPad Mini"]
+    amazonTablets = ["Fire HD 10","Fire HD 8","Fire 7","Fire 10 Kids","Fire 10 Kids","Fire 8 Kids","Fire 8 Pro Kids"]
+    HPtablets = ["11 inch","Chromebook"]
+    lenovoTablets = ["Tab M10 Plus","Windows Duet 3i","Chromebook Duet 5","Tab P11 Plus","Tab M8","Yogo Tab 11","Tab M10","Tab P11","Tab P12 Pro"]
+    microsoftTablets = ["Surface Pro 8","Surface Pro 7","Surface Pro 7+","Surface Pro X","Surface Go 3"]
+    samsungTablets = ["Galaxy Tab A7","Galaxy Tab A8","Galaxy Tab A7","Galaxy Tab S6 Lite","Galaxy Tab S8 Ultra","Galaxy Tab A7 Lite","Galaxy Tab S8+","Galaxy Tab S7 Plus"]
+
+}
+
 
 
 class Televisions:
+
+function majorTeles: {
+
+}
