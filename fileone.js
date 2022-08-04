2c9c133da4270a880954a445ea619ab2d1bc2401
Marvish <mschandr@uci.edu>
2022-08-04 14:36:17 -0700

    Added input response questions for the Phone class.

--- fileone.js
+++ fileone.js
@@ -7,6 +7,8 @@ constructor(technology){
 PhoneProducts = function(){
 const ioSProducts = ["iPhone 13", "iPhone 12","iPhone SE", "iPhone 11", "iPhone X"]
 askIOS = <input type="What ioS product have you decided to purchase?" />
+    for ios in ioSProducts:
+    print(ioSProducts[ios])
     if (askIOS === ioSProducts[0]){
     document.getElementById("demo").innerHTML = "You have decided to purchase the latest iPhone model."}
     if (askIOS === ioSProducts[1]){
@@ -26,7 +28,9 @@ askIOS = <input type="What ioS product have you decided to purchase?" />
     }
 
 const samsungProducts = ["Samsung Galaxy S22","Samsung Galaxy S21","Samsung Galaxy Z Flip 3", "Samsung Galaxy Z Fold 3"]
-askSamsung = <input type="What Samsung product have you decided to purchase>?" />}
+askSamsung = <input type="What Samsung product have you decided to purchase>?" />
+for sam in samsungProducts:
+print(samsungProducts[sam])
 if (askSamsung === samsungProducts[0]){
     document.getElementById("demo").innerHTML = "You have decided to purchase the latest Samsung Galaxy model."}
 if (askSamsung === samsungProducts[1]){
@@ -41,6 +45,8 @@ else{
 }
 
 const googleProducts = ["Google Pixel 6a","Google Pixel 6","Google Pixel 6 Pro"]
+for g in googleProducts:
+print(googleProducts[g])
 askGoogle = <input type="What Google product have you decided to purchase?"/>}
 if (askGoogle === googleProducts[0]){
     document.getElementById("demo").innerHTML = "You have decided to purchase the latest Google Pixel model."
@@ -59,18 +65,57 @@ class Phone extends Computers:
 
 function majorComps(){
     appleProducts = ["Macbook Air (M2)","Macbook Air (M1)","Macbook Pro 13 (M2)", "Macbook Pro 13 (M1)", "Macbook Pro 14","Macbook Pro 16"]
+    for apple in appleProducts:
+    print(appleProducts[apple])
+    askApple = <input type={"What Apple product have you decided to purchase"/>}
     acerProducts = ["Aspire 3", "Aspire 5","Nitro 5","Predator","Chromebook"]
+    for acer in acerProducts:
+    print(acerProducts[acer])
+    askAcer = <input type={"What Acer product have you decided to purchase"/>}
     alienwareProducts = ["x15 R1","m15 R5", "m17 R5", "x17 R1", "x14 R1", "m15 R4", "m15 R7", "x15 R2"]
+    for alien in alienwareProducts:
+    print(alienwareProducts[alien])
+    askAlien = <input type={{"What Alien product have you decided to purchase?"/>}
     asusProducts = ["Vivobook","Zenbook","ROG","ROG Zephyrus","TUF Dash", "Vivobook Pro","Zenbook Pro","Chromebook"]
+    for asus in asusProducts:
+    print(asusProducts[asus])
+    askAsus = <input type={"What Asus product have you decided to purchase?"/>}
     dellProducts = ["Inspiron","G15","XPS 13", "XPS 17","Latitude"]
+    for dell in dellProducts:
+    print(dellProducts[dell])
+    askDell = <input type={"What Dell product have you decided to purchase?"/>}
     gigabyteProducts = ["A5","Aero","Aorus","Aorus Level","U4","G5"]
+    for giga in gigabyteProducts:
+    print(gigabyteProducts[giga])
+    askGiga = <input type={"What Gigabyte product have you decided to purchase?"/>}
     HPproducts = ["Envy","Stream","Spectre","Omen","Pavilion","Elitebook","Victus"]
+    for hp in HPproducts:
+    print(HPproducts[hp])
+    askHP = <input type={"What HP product have you decided to purchase?"/>}
     lenovoProducts = ["Yoga 7i","Ideapad","Yoga 6","Flex","Chromebook","Thinkpad","Legion"]
+    for len in lenovoProducts:
+    print(lenovoProducts[len])
+    askLen = <input type={"What Lenovo product have you decided to purchase?"/>}
     lgProducts = ["Gram 17","Gram 15","Gram WUXUGA"]
+    for lg in lgProducts:
+    print(lgProducts[lg])
+    askLG = <input type={"What LG product have you decided to purchase?"/>}
     microsoftProducts = ["Surface","Surface Pro X","Surface 2-in-1"]
+    for micro in microsoftProducts:
+    print(microsoftProducts[micro])
+    askMicro = <input type={"What Micosoft product have you decided to purchase?"/>}
     msiProducts = ["Modern 15","Prestige","Sword","Delta","Stealth","Vector","GP66","GF65"]
+    for msi in msiProducts:
+    print(msiProducts[msi])
+    askMSI = <input type={"What MSI product have you decided to purchase?"/>}
     razerProducts = ["Blade 14","Blade 15","Book 13","Blade 17","Blade 15.6","Blade Stealth 13.3"]
+    for razer in razerProducts:
+    print(razerProducts[razer])
+    askRazer = <input type={"What Razer product have you decided to purchase"/>}
     samProducts = ["Galaxy Book Go","Galaxy Book 2 Pro","Galaxy Book Flex 2","Galaxy Book Pro 360","Chromebook", "Galaxy Book Odyssey"]
+    for sam in samProducts:
+    print(samProducts[sam])
+    askSam = <input type{"What Samsung product have you decided to purchase?"/>}
 }
 
 
@@ -78,11 +123,23 @@ class Phones class Tablets(){
 
 function majorTabs: {
     appleTablets = ["iPad Air","iPad Pro","10.2in iPad","iPad Mini"]
+    for apple in appleTablets:
+        print(appleTablets[apple])
     amazonTablets = ["Fire HD 10","Fire HD 8","Fire 7","Fire 10 Kids","Fire 10 Kids","Fire 8 Kids","Fire 8 Pro Kids"]
+    for ama in amazonTablets:
+        print(amazonTablets[ama])
     HPtablets = ["11 inch","Chromebook"]
+    for h in HPtablets:
+        print(HPtablets[h])
     lenovoTablets = ["Tab M10 Plus","Windows Duet 3i","Chromebook Duet 5","Tab P11 Plus","Tab M8","Yogo Tab 11","Tab M10","Tab P11","Tab P12 Pro"]
+    for l in lenovoProducts:
+        print(lenovoProducts[l])
     microsoftTablets = ["Surface Pro 8","Surface Pro 7","Surface Pro 7+","Surface Pro X","Surface Go 3"]
+    for m in microsoftTablets:
+        print(microsoftTablets[m])
     samsungTablets = ["Galaxy Tab A7","Galaxy Tab A8","Galaxy Tab A7","Galaxy Tab S6 Lite","Galaxy Tab S8 Ultra","Galaxy Tab A7 Lite","Galaxy Tab S8+","Galaxy Tab S7 Plus"]
+    for s in samsungTablets:
+        print(samsungTablets[s])
 
 }
 
@@ -98,6 +155,6 @@ function majorTeles(): {
     samsungTV = ["Class 7 Series","Class 5 Series","QN85B","Q70A","8000 Series","S95B","Q60B","Q80A","Q80B","QN90A","QN90B","TU8300","M4500","Q60T","N5300","Q60A","BE50T-H"]
     sonyTV =["X80K,""X85K","X95J","A80K","A90K","A95K","A9S","X75K","A80J","X85J","X90J","A90J","X95K","X80J","W830K","X91J","Z9K","Z9J","A8H"]
     tclTV = ["43S455","Class 3 Series","Class 4 Series","Class 5 Series","Class 6 Series","Class XL"]
-    toshibaTV = []
-    vizioTV = []
+    toshibaTV = ["C350","V35","M550"]
+    vizioTV = ["D Series","M7","V Series","M6 Series","P Series","MQ6 Series","MQX Series"]
 }
